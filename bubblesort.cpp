23a6f3d2a2c3fc80311f3f729adc049788aab074
Shubham Vishwakarma <153703088+ShubhamViswa@users.noreply.github.com>
2024-05-09 02:57:01 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,3 +1,22 @@
+// Question ----> Implement Bubble Sort
+
+/** 
+_____________________________________________________
+Write a C++ program to implement bubble sort strictly as describe in the class on 07.05.2024. The program should do the following:
+
+1. Ask the user to enter N (total numbers to sort)
+2. Ask the user to enter N integer numbers.
+3. Displays the Unsorted Sequence and Sorted sequence one after another.
+
+Example:
+
+Enter N: 6
+Enter 6 integer numbers: 34 56 8 14 10 7
+Unsorted sequence: 34 56 8 14 10 7
+....Sorted Sequence: 7 8 10 14 34 56
+______________________________________________________
+**/
+
 #include <iostream>
 
 using namespace std;
